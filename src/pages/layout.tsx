b47cfc54a19525c066c54c831babaeac14a905b0
yihyun-kim1 <hyunnyfarm@naver.com>
2024-02-04 15:35:30 +0900

    배포 관련 빌드 에러 수정 layout.tsx
    - use client 추가
    - globals.css 경로 수정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,7 @@
+'use client'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../pages/globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
